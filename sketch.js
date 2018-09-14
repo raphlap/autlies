3cc0695e537b3acd2ed2dda4b4d8307c48849a70
raphlap <raphaelle.lapotre@gmail.com>
2018-09-14 18:26:17 +0200

    added ellipses with css stylesheet and prepared tooltips

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ let width = 1000;
 let height = 500;
 let cuy = 0;
 let cux = 0;
+let posx;
+let posy;
+let tooltip;
 function preload() {
     //data = loadJSON('~/data.bnf.fr/visu_auteurs_lies/auteursliesmusset.json')
 }
@@ -26,10 +29,11 @@ function setup() {
         }  
         })
     for (var key in data){
+        cux += Object.keys(data[key]).length;
         Object.keys(data[key]).forEach(function(k){
             count += 1;
             cuy = 0;
-            cux += Object.keys(data[key]).length;
+            
             coord[k + count.toString()] = [];
             coord[k + count.toString()].push({'role' : key, 'index' : Object.keys(data[key]).indexOf(k), 'name': k, 'tx' : cux})
             for (var i in diclet) {
@@ -48,7 +52,10 @@ function setup() {
         // }
     }
         //console.log(coord)
+        console.log(coord['Ã‰mile Faguet1565']);
         createCanvas(width, height);
+        //var tooltip = selectAll('.tooltip');
+        
 }
 
 function draw() {
@@ -74,12 +81,36 @@ function draw() {
         
         line(0, 0, lont/2, 0)
     }
-    //console.log(coord);
+    
     for (var el in coord) {
-        push();
-        translate(el[1]['cux']/2, el[0]['cuy']/2);
-        pop();
-        point (el[1]['index']/5, el[0]['index']/5);
-
+        //console.log(el)
+        //
+        if (coord[el][1] !== undefined) {
+            //console.log('yes !')
+            //console.log(coord[el][1]['ty'], coord[el][0]['tx'])
+        //push();
+        //translate(coord[el][0]['tx']/2, coord[el][1]['ty']/5);
+        
+        //stroke(0);
+        posx = coord[el][0]['tx']/2+coord[el][0]['index']/2;
+        posy = coord[el][1]['ty']/5+coord[el][1]['index']/5;
+        div = createDiv();
+        div.size(5,5);
+        //div.style('translate', coord[el][0]['tx']/2, coord[el][1]['ty']/5);
+        div.style('border', '0.5px solid #000');
+        div.style('border-radius', '5px / 5px');
+        div.position(posx,posy);
+        //div.mouseOver(showtooltip(posx, posy));
+        //ellipse(coord[el][0]['index']/2, coord[el][1]['index']/5, 5, 5);
+        //pop();
+        }else{
+            //console.log(el)
+        }
     }
+}
+
+function showtooltip(x, y) {
+    // tooltip = select('.tooltip');
+    // tooltip.style('display', 'block');
+    // tooltip.position(x, y);
 }
