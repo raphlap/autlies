ab7120772ab8991b10ca4ab5caf801c08b0581aa
raphlap <raphaelle.lapotre@gmail.com>
2018-09-17 18:17:13 +0200

    tooltips on their way

--- sketch.js
+++ sketch.js
@@ -11,12 +11,14 @@ let cux = 0;
 let posx;
 let posy;
 let tooltip;
+let id;
+let selected;
 function preload() {
     //data = loadJSON('~/data.bnf.fr/visu_auteurs_lies/auteursliesmusset.json')
 }
 
 function setup() {
-    noLoop()
+    //noLoop()
     //console.log(data)
     letters.forEach(function(l){
         diclet[l] = [];
@@ -45,16 +47,41 @@ function setup() {
         })
     }
 
-    for (var key in coord) {
-        //console.log(typeof key)
-        // if (key.charAt(0) == 0){
-        //     delete coord.key;
-        // }
+    for (var el in coord) {
+        if (coord[el][1] !== undefined) {
+            id = el;
+            posx = coord[el][0]['tx']/2+coord[el][0]['index']/2;
+            posy = coord[el][1]['ty']/5+coord[el][1]['index']/5;
+            tooltip = createDiv('coucou !');
+            tooltip.style('display', 'none');
+            tooltip.addClass('tooltip');
+            tooltip.id(id);
+            tooltip.position(posx, posy);
+            div = createDiv();
+            div.size(5,5);
+            div.style('border', '0.5px solid #000');
+            div.style('border-radius', '5px / 5px');
+            div.position(posx,posy);
+            div.addClass('ellipse')
+            div.id(id);
+            
+            //div.mouseOut(hidetooltip);
+
+        }
+    
     }
         //console.log(coord)
-        console.log(coord['Ã‰mile Faguet1565']);
+        //console.log(coord['Ã‰mile Faguet1565']);
         createCanvas(width, height);
         //var tooltip = selectAll('.tooltip');
+        var ellipses = selectAll('.ellipse');
+        //console.log(ellipses);
+        for (var i = 0; i<ellipses.length; i++){
+            //console.log(ellipses[i]['elt']);
+            selected = ellipses[i].mouseOver();
+            // ellipses[i].mouseOut(hidetooltip);
+            //console.log(selected['elt'].id)
+        }
         
 }
 
@@ -82,35 +109,30 @@ function draw() {
         line(0, 0, lont/2, 0)
     }
     
-    for (var el in coord) {
-        //console.log(el)
-        //
-        if (coord[el][1] !== undefined) {
-            //console.log('yes !')
-            //console.log(coord[el][1]['ty'], coord[el][0]['tx'])
-        //push();
-        //translate(coord[el][0]['tx']/2, coord[el][1]['ty']/5);
-        
-        //stroke(0);
-        posx = coord[el][0]['tx']/2+coord[el][0]['index']/2;
-        posy = coord[el][1]['ty']/5+coord[el][1]['index']/5;
-        div = createDiv();
-        div.size(5,5);
-        //div.style('translate', coord[el][0]['tx']/2, coord[el][1]['ty']/5);
-        div.style('border', '0.5px solid #000');
-        div.style('border-radius', '5px / 5px');
-        div.position(posx,posy);
-        //div.mouseOver(showtooltip(posx, posy));
-        //ellipse(coord[el][0]['index']/2, coord[el][1]['index']/5, 5, 5);
-        //pop();
-        }else{
-            //console.log(el)
-        }
-    }
+    // for (var el in coord) {
+    //     //console.log(el)
+    //     //
+    //     // if (coord[el][1] !== undefined) {
+    //     // posx = coord[el][0]['tx']/2+coord[el][0]['index']/2;
+    //     // posy = coord[el][1]['ty']/5+coord[el][1]['index']/5;
+ 
+
+    //      //}
+         
+    // }
+
 }
 
-function showtooltip(x, y) {
-    // tooltip = select('.tooltip');
-    // tooltip.style('display', 'block');
-    // tooltip.position(x, y);
+function mouseOver() {
+    console.log(selected);
+    //tooltip = select('#'+ id);
+    
+    //tooltip.style('display', 'block');
+    
+}
+
+function hidetooltip(){
+    tooltip = select('#'+ id);
+    
+    tooltip.style('display', 'none');
 }
